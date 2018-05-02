69d8cf08592eca529b40776b81fe42ea3c8a1891
whartsell <will@willshouse.net>
2018-05-02 14:14:27 -0700

    compundSeriesHelper from resource is working

--- vJMPS/vJMPS/Core/SeriesHelpers.cs
+++ vJMPS/vJMPS/Core/SeriesHelpers.cs
@@ -1,18 +1,29 @@
 using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 namespace vJMPS.Core
 {
     public static class SeriesHelpers
     {
-        public static ChartSeries ChartSeriesFromResourceJSON(string assembly,string resourcePath, string seriesName)
+        public static CompoundChartSeries CompoundChartSeriesFromResourceJSON(string assembly,string resourcePath, string seriesName)
         {
-          
+            CompoundChartSeries results = new CompoundChartSeries();
             using (Stream stream = Assembly.LoadFrom(assembly).GetManifestResourceStream(resourcePath))
             using (StreamReader reader = new StreamReader(stream))
             {
+
                 JObject obj = JObject.Parse(reader.ReadToEnd());
-                return new ChartSeries();
+                Dictionary<string, Dictionary<string,double[]>> elements = obj[seriesName].ToObject<Dictionary<string, Dictionary<string,double[]>>>();
+                foreach (KeyValuePair<string, Dictionary<string,double[]>> item in elements)
+                {
+                    double[] xValues = item.Value["x"];
+                    double[] yValues = item.Value["data"];
+                    results.Add(Double.Parse(item.Key), new ChartSeries(xValues, yValues));
+                    
+                }
+                return results;
             }
             
         }
