8608571c967b844787eeea39189eff327a551d43
whartsell <will@willshouse.net>
2018-05-10 23:03:49 -0700

    added IOC deps

---
+++ vJMPS/vJMPS/IOCSetup.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vJMPS
+{
+    class IOCSetup
+    {
+        public IContainer CreateContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+            //something magical here
+
+            return containerBuilder.Build();
+        }
+    }
+}
