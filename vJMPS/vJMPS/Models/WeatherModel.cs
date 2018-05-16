dee44e963d1459f7ae10b4c2f97a3f120569ffc6
whartsell <will@willshouse.net>
2018-05-15 23:08:50 -0700

    gotta figure out wind and weather

---
+++ vJMPS/vJMPS/Models/WeatherModel.cs
@@ -0,0 +1,12 @@
+﻿
+using vJMPS.ViewModels;
+
+namespace vJMPS.Models
+{
+    class WeatherModel : ViewModelBase
+    {
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        public WindModel  Wind { get; set; }
+    }
+}
---
+++ vJMPS/vJMPS/Models/Wind.cs
@@ -0,0 +1,8 @@
+﻿namespace vJMPS.Models
+{
+    public class WindModel
+    {
+        public int Direction { get; set; }
+        public int Magnitude { get; set; }
+    }
+}
